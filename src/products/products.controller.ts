57561e0ff45c1b6a2cbd9e56e5da97528cf62dae
ozbeyahmet1 <ahmet1ozbey@gmail.com>
2024-12-29 00:34:11 +0300

    feat: implements crud operaitons for products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':href')
+  findOne(@Param('href') href: string) {
+    return this.productsService.findOne(href);
+  }
+
+  @Post()
+  create(@Body() createProductDto: any) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() updateProductDto: any) {
+    return this.productsService.update(+id, updateProductDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.productsService.remove(+id);
+  }
+}
