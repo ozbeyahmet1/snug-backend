13fc8318d99d50ac147bf07ca361b6e42dc5ebef
ozbeyahmet1 <ahmet1ozbey@gmail.com>
2024-12-29 00:35:55 +0300

    feat: adds swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
+  const config = new DocumentBuilder()
+  .setTitle('E-commerce API')
+  .setDescription('API for Products and Reviews')
+  .setVersion('1.0')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
